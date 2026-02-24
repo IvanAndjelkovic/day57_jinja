cfe1e836f514ba2f07da2d89c43e880c13abddb0
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-24 13:06:03 +0000

    Finishing the age and gender api connection to the index html file and server.py

--- server.py
+++ server.py
@@ -7,11 +7,11 @@ import requests
 
 app=Flask(__name__)
 
-@app.route("/")
-def home():
-    random_number = random.randint(1, 10)
-    current_year=datetime.datetime.now().year
-    return  render_template("index.html", num=random_number, current_year=current_year)
+# @app.route("/")
+# def home():
+#     random_number = random.randint(1, 10)
+#     current_year=datetime.datetime.now().year
+#     return  render_template("index.html", num=random_number, current_year=current_year)
 
 @app.route("/guess/<name>")
 def guess_age(name):
@@ -20,7 +20,8 @@ def guess_age(name):
 
     age=response_age.json()['age']
     gender=response_gender.json()['gender']
-    return f'My age is {age} and you are probable {gender}'
+
+    return  render_template("index.html", age=age, gender=gender, name=name)
 
 
 
