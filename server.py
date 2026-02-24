fa443806dd1e8bd6169055802aee003d31c4aacd
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-24 07:37:47 +0000

    Entering a name route with the api for checking the age and gender

--- server.py
+++ server.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+import datetime
+import requests
+
+
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    
-    return  "Hello World"
+    random_number = random.randint(1, 10)
+    current_year=datetime.datetime.now().year
+    return  render_template("index.html", num=random_number, current_year=current_year)
+
+@app.route("/guess/<name>")
+def guess_age(name):
+    response_age=requests.get(f"https://api.agify.io?name={name}")
+    response_gender=requests.get(f"https://api.genderize.io?name={name}")
+
+    age=response_age.json()['age']
+    gender=response_gender.json()['gender']
+    return f'My age is {age} and you are probable {gender}'
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
