84539be9335eec40e5f6256334b8cd554c84ed8c
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-26 07:00:59 +0000

    Adding one more attribute tot the blog page, and also adding a link to the blog page from the index page. The blog page will now show only one post, which is the one with id 2.

--- server.py
+++ server.py
@@ -7,11 +7,11 @@ import requests
 
 app=Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     random_number = random.randint(1, 10)
-#     current_year=datetime.datetime.now().year
-#     return  render_template("index.html", num=random_number, current_year=current_year)
+@app.route("/")
+def home():
+    random_number = random.randint(1, 10)
+    current_year=datetime.datetime.now().year
+    return  render_template("index.html", num=random_number, current_year=current_year)
 
 @app.route("/guess/<name>")
 def guess_age(name):
@@ -22,12 +22,12 @@ def guess_age(name):
     gender=response_gender.json()['gender']
 
     return  render_template("index.html", age=age, gender=gender, name=name)
-@app.route("/blog")
-def blog():
+@app.route("/blog/<num>")
+def get_blog(num):
     blog_url="https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts=response.json()
-    return render_template("blog.html", posts=all_posts)
+    return render_template("blog.html", posts=all_posts, post_number=num)
 
 
 
